f050d727ebdf3cccd9e9f831de41fc44b5fe96a3
Vincent Howard <vincenguyenhoward@gmail.com>
2024-01-24 16:56:42 -0700

    created sign up endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Elysia, t } from "elysia";
-import { cookie } from "@elysiajs/cookie";
 import { jwt } from "@elysiajs/jwt";
 import { prisma } from "./services/prisma";
 import { User } from "./types";
@@ -9,59 +8,73 @@ const app = new Elysia()
   .decorate("db", prisma)
   .state("version", 1.0)
   .use(jwt(jwtConfig))
-  .use(cookie())
   .post(
     "/users/signup",
     async ({ body, set, db, jwt }) => {
       const { username, email, password } = body as User;
-      const hashedPassword = await Bun.password.hash(password);
-      const emailExists = await prisma.user.findUnique({
-        where: {
-          email,
-        },
-        select: {
-          id: true,
-        },
-      });
+      try {
+        const hashedPassword = await Bun.password.hash(password);
+        const emailExists = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+          select: {
+            id: true,
+          },
+        });
 
-      if (emailExists) {
-        set.status = 400;
+        if (emailExists) {
+          set.status = 400;
+          return {
+            success: false,
+            data: null,
+            message: "Email address already in use.",
+          };
+        }
+
+        const usernameExists = await prisma.user.findUnique({
+          where: {
+            username,
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (usernameExists) {
+          set.status = 400;
+          return {
+            success: false,
+            data: null,
+            message: "Someone already taken this username.",
+          };
+        }
+
+        const user = await db.user.create({
+          data: {
+            username: username,
+            password: hashedPassword,
+            email: email,
+          },
+        });
+
+        const token = await jwt.sign({
+          userId: user.id,
+        });
+
+        return {
+          success: true,
+          token,
+          message: "Account created",
+        };
+      } catch (error) {
+        set.status = 500;
         return {
           success: false,
           data: null,
-          message: "Email address already in use.",
+          message: "Something went wrong.",
         };
       }
-
-      const usernameExists = await prisma.user.findUnique({
-        where: {
-          username,
-        },
-        select: {
-          id: true,
-        },
-      });
-
-      if (usernameExists) {
-        set.status = 400;
-        return {
-          success: false,
-          data: null,
-          message: "Someone already taken this username.",
-        };
-      }
-
-      const user = await db.user.create({
-        data: {
-          username: username,
-          password: hashedPassword,
-          email: email,
-        },
-      });
-
-      return {
-        token: jwt.sign({ id: user.id }),
-      };
     },
     {
       body: t.Object({
